7e1c8a4910d137ee00cb0500244c20221ea6042a
Thomas Sanger <thomas.sanger92@gmail.com>
2025-03-23 14:10:27 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector("#addBtn").addEventListener("click", () => {
+  let destinations = new Map();
+  let destinationUl = document.querySelector("#destinationUl");
+  let txtAdd = document.querySelector("#txtAdd");
+});
