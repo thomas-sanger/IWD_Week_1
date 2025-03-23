d543c8dff21bbb785544286a1eb291d7fe1e8f86
Thomas Sanger <thomas.sanger92@gmail.com>
2025-03-23 14:30:21 -0600

    Finished project

--- js/main.js
+++ js/main.js
@@ -2,4 +2,19 @@ document.querySelector("#addBtn").addEventListener("click", () => {
   let destinations = new Map();
   let destinationUl = document.querySelector("#destinationUl");
   let txtAdd = document.querySelector("#txtAdd");
+
+  if (txtAdd.value.length > 0) destinations.set(Date.now(), txtAdd.value);
+
+  for (let [key, value] of destinations) {
+    let item = document.createElement("li");
+    item.innerText = `${value}`;
+    destinationUl.append(item);
+    document.querySelector("#txtAdd").value = "";
+  }
+});
+
+document.querySelector("#clearBtn").addEventListener("click", () => {
+  let destinationUl = document.querySelector("#destinationUl");
+  destinationUl.innerText = "";
+  document.querySelector("#txtAdd").value = "";
 });
